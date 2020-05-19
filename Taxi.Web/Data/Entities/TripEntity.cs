656a8465820f175eee29da75823037cd62a16bf0
emersonpalacio <emersonpalaciootalvaro@gmail.com>
2020-05-19 13:00:46 -0500

    positbility

--- Taxi.Web/Data/Entities/TripEntity.cs
+++ Taxi.Web/Data/Entities/TripEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -10,7 +11,21 @@ namespace Taxi.Web.Data.Entities
     {
         public int Id { get; set; }
 
-       
+        [DataType(DataType.Date)]
+        [Display(Name = "Start Date")]
+        [DisplayFormat(DataFormatString ="{0: yyyy:MM:dd}", ApplyFormatInEditMode =false)]
+        public DateTime StartDate{ get; set; }
+
+        public DateTime StartDateLocalTime => StartDate.ToLocalTime();
+
+        [DataType(DataType.Date)]
+        [Display(Name ="End date")]
+        [DisplayFormat(DataFormatString ="{0: yyyy:MM:dd}", ApplyFormatInEditMode =false)]
+        public DateTime EndDate { get; set; }
+
+        public DateTime EndDateLocal => EndDate.ToLocalTime();
+
+
 
     }
 }
