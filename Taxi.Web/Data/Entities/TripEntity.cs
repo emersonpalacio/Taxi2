5f56130bebaa8d4fb1ed6d922d300fea7b617fda
emersonpalacio <emersonpalaciootalvaro@gmail.com>
2020-05-19 12:44:15 -0500

    firstcommin

---
+++ Taxi.Web/Data/Entities/TripEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Taxi.Web.Data.Entities
+{
+    public class TripEntity
+    {
+        public int Id { get; set; }
+
+       
+
+    }
+}
---
+++ Taxi.Web/Migrations/20200519172010_interrup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Taxi.Web.Migrations
+{
+    public partial class interrup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
