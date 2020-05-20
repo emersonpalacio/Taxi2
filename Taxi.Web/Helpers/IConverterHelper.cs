9991ef9109202efbeb13fd8aa16a7e4aeea8dc99
emersonpalacio <emersonpalaciootalvaro@gmail.com>
2020-05-20 05:54:16 -0500

    FInallyUserLogin

---
+++ Taxi.Web/Helpers/IConverterHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taxi.Web.Helpers
+{
+    public class IConverterHelper
+    {
+    }
+}
